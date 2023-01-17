61d58e06dc7951c6ae95c66a1d22f70e9beb8bd7
BPVG <93517364+BPVG@users.noreply.github.com>
2023-01-17 14:02:11 +0200

    Add project files.

---
+++ PecIzveles/Form1.cs
@@ -0,0 +1,47 @@
+namespace PecIzveles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            checkBox1.Hide();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                checkBox1.Show();
+            } else
+            {
+                checkBox1.Hide();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
